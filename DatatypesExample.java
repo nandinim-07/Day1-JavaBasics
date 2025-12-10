cf23333600d0e17f103b61fa9ff3722e3abcc8c5
nandinim-07 <nandinimunukanpally@gmail.com>
2025-12-10 15:19:05 +0530

    Add files via upload

---
+++ DatatypesExample.java
@@ -0,0 +1,25 @@
+public class DatatypesExample {
+    public static void main(String[] args) {
+        byte byteNum = 10;          
+        short shortNum = 200;       
+        int intNum = 5000;          
+        long longNum = 150000L;     
+
+        float floatNum = 5.75f;     
+        double doubleNum = 19.99;   
+
+        char letter = 'A';          
+        boolean isJavaFun = true;   
+        String name = "Nandini";    
+        System.out.println("byte: " + byteNum);
+        System.out.println("short: " + shortNum);
+        System.out.println("int: " + intNum);
+        System.out.println("long: " + longNum);
+        System.out.println("float: " + floatNum);
+        System.out.println("double: " + doubleNum);
+        System.out.println("char: " + letter);
+        System.out.println("boolean: " + isJavaFun);
+        System.out.println("String: " + name);
+    }
+}
+
---
+++ VariablesExample.java
@@ -0,0 +1,12 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 20;              
+        double price = 99.99;      
+        String name = "Nandini";   
+        boolean isStudent = true;  
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
